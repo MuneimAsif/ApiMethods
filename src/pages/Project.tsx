69926dae64ad8e0ee001fbd42f4bf348f6d59629
MuneimAsif <127970035+MuneimAsif@users.noreply.github.com>
2023-09-15 17:09:19 +0500

    Update Project.tsx

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -1,16 +1,14 @@
-// import { Delete } from "@mui/icons-material"
 import { Box, IconButton, Typography } from "@mui/material"
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
-import axios from "axios"
 import { useState,useEffect } from "react"
 import {useNavigate} from 'react-router-dom'
-import AddProject from "./AddProject";
+import { Get,Del}  from "../confIg/apiHandling";
 export default function Project(){
     const [model,setModel] = useState<any>([])
     const navigate = useNavigate()
     const getData = () => {
-        axios.get('https://jsonplaceholder.typicode.com/comments')
+        Get(`comments`)
         .then((res)=>{
         console.log(res.data);
           setModel([...res.data])
@@ -20,7 +18,7 @@ export default function Project(){
         })
     }
     const deletePost = (id:any)=>{
-        axios.delete(`https://jsonplaceholder.typicode.com/comments/${id}`)
+        Del(`comments/${id}`)
         .then(()=>{
          console.log('Post Deleted Successfully');
          
@@ -30,7 +28,10 @@ export default function Project(){
     }
     useEffect(()=>{getData()},[])
     return <>
-    <h1 className="text-center my-5 ">PROJECTS</h1>
+    <div className="text-center"> 
+    <h1 className="my-5 ">PROJECTS</h1>
+    <button className="btn bg-primary text-white" onClick={()=>navigate('/AddProject')}>Add Your Own Data</button>
+    </div>
          {
             model.map((x:any,i:any)=>
             <Box key={i} className = 'mt-3 p-5 mx-auto border rounded  w-50'>
@@ -38,7 +39,7 @@ export default function Project(){
                   <Typography className="mt-2" ><b>NAME :</b> {x.name}</Typography>
                   <Typography className="mt-2" ><b>EMAIL :</b> {x.email}</Typography>
                   <IconButton aria-label="delete" color = "error" onClick={()=>{deletePost(x.id)}} ><DeleteIcon></DeleteIcon></IconButton>
-                  <IconButton aria-label="edit" color = "primary" onClick={()=> navigate(`AddProject/${x.id}`)} ><EditIcon></EditIcon></IconButton>
+                  <IconButton aria-label="edit" color = "primary" onClick={()=> navigate(`/AddProject/${x.id}`)} ><EditIcon></EditIcon></IconButton>
             </Box>)
          }
      </>
