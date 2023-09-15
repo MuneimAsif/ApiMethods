a3d687f7da3f748f102fa173850e2c0d4b9a0467
MuneimAsif <127970035+MuneimAsif@users.noreply.github.com>
2023-09-15 17:07:55 +0500

    Update apiHandling.tsx

--- src/confIg/apiHandling.tsx
+++ src/confIg/apiHandling.tsx
@@ -1,1 +1,20 @@
+import axios from "axios";
 
+export const apiHandle = axios.create({
+    baseURL: 'https://jsonplaceholder.typicode.com'
+})
+
+export const Get = (epOfGet: string) => {
+    return apiHandle.get(epOfGet)
+}
+export const Del = (epOfDel: string, id?: (string | number)) => {
+    return apiHandle.delete(`${epOfDel}/${id ? id : ""}`)
+}
+
+export const Put = (epOfEdit:string, id?:(string | number),model?:{postId:(number|string),name:string,email:string}) => {
+    return apiHandle.put(`${epOfEdit}/${id ? id : ""}`,model?model:"")
+}
+
+export const Post = (eoOfPost:string,model:{postId:number,name:string,email:string})=>{
+ return apiHandle.post(`${eoOfPost}`,model)
+}
