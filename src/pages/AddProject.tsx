58977e72866c2db74c1e6ff2b4ea21db072e1a0d
MuneimAsif <127970035+MuneimAsif@users.noreply.github.com>
2023-09-15 17:08:54 +0500

    Update AddProject.tsx

--- src/pages/AddProject.tsx
+++ src/pages/AddProject.tsx
@@ -1,49 +1,69 @@
-import {Box,Button,TextField,Paper} from '@mui/material'
-import {useParams} from 'react-router-dom'
-import {useEffect,useState} from 'react'
+import { useParams } from 'react-router-dom'
+import { useEffect,useState } from 'react'
+import { Post,Put } from '../confIg/apiHandling';
 import axios from 'axios'
 
-export default function AddProject(){
-    const baseApi = 'https://jsonplaceholder.typicode.com/comments';
-    const [model,setModel] = useState<any>({})
-    const params = useParams();
-    const getDataById = ()=>{
-        axios.get(`${baseApi}/${params.id}`)
-        .then((res)=>{
-            console.log(res.data);
-            setModel({...res.data})
-            }).catch((err)=>{
-            console.log(err.data);
-            
-        })
+export default function AddProject() {
+  // const baseApi = 'https://jsonplaceholder.typicode.com/comments';
+  const [model, setModel] = useState<any>({})
+  const params = useParams();
+  
+  const getDataById = () => {
+  axios.get(`https://jsonplaceholder.typicode.com/comments/${params.id}`).then((res) => {
+        console.log(res.data);
+        setModel({ ...res.data })
+      }).catch((err) => {
+        console.log(err.data);
+
+      })
+  }
+
+  const submitPost = () => {
+    Post('comments', model)
+      .then((res) => {
+        console.log(`Post Added ===> ${res.data}`);
+
+      }).catch((err) => {
+        console.log(err.data);
+
+      })
+  }
+
+  const updatePost = () => {
+    Put(`comments/${params.id}`)
+      .then((res) => {
+        console.log(`Post Updated === > ${res.data}`);
+
+      }).catch((err) => {
+        console.log(err.data);
+      }) 
+  }
+
+
+  useEffect(() => {
+
+    if (params.id) {
+      getDataById()
     }
+  }, [])
+  return <>
 
-       const updatePost = ()=>{
-        axios.put(`${baseApi}/${params.id}`,model)
-        .then((res)=>{
-     console.log(`Post Updated === > ${res.data}`);
-     
-        }).catch((err)=>{
-     console.log(err.data);
-        })
-       }
-
-       useEffect(() =>
-       {if(params.id){
-        getDataById()
-       }},[])
-    return <>
-    
-     <Box className = 'text-center'>
-      <Paper className='p-3 mx-auto mt-5 w-50'>
-        <Box><TextField value={model.postId} variant='outlined' placeholder = 'Post ID' className='my-2' type='number'></TextField></Box>
-        <Box><TextField value={model.name} variant='outlined' placeholder = 'Name' className='my-2' type='text'></TextField></Box>
-        <Box><TextField value={model.email} variant='outlined' placeholder = 'Email' className='my-2' type='email'></TextField></Box>
-      <Button onClick={updatePost} >Update Post</Button>
-      </Paper>
-    </Box>    
+    <div className='d-flex mt-5'>
+      <div className='mx-auto  mt-5 border p-5'>
+        <div><input value={model.postId} onChange={(e) => setModel({ ...model, postId: e.target.value })} className='my-3 border-light' type="number" /></div>
+        <div><input value={model.name} onChange={(e) => setModel({ ...model, name: e.target.value })} className='my-3 border-light' type="text" /></div>
+        <div><input value={model.email} onChange={(e) => setModel({ ...model, email: e.target.value })} className='my-3 border-light' type="email" /></div>
+        <div className='text-center'><button onClick={submitPost} className='btn bg-primary text-white my-3' >Submit</button></div>
 
 
-    
-    </>
+        <div className='text-center'><button onClick={updatePost} className='btn bg-primary text-white my-2' >Update</button></div>
+
+      </div>
+
+    </div>
+
+
+
+
+  </>
 }
